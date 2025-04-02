59590c0a956b8ed4416c929f9c829e1c6025d8ec
Example User <example@example.com>
2025-04-02 15:15:24 +0530

    first commit

---
+++ src/main.py
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.apache.commons.lang3.StringUtils;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleApp {
+    public static void main(String[] args) {
+        String text = "Hello, World!";
+        System.out.println(StringUtils.reverse(text));
+        
+        List<String> words = new ArrayList<String>();
+        words.add("Java");
+        words.add("Programming");
+        
+        for (String word : words) {
+            System.out.println(StringUtils.capitalize(word));
+        }
+    }
+}
