6937519df19c4497d38169dcd6c23b854ed1bdfe
shreyasarjun <shreyas.arjun007@gmail.com>
2025-04-04 12:30:38 +0530

    Update test.java

--- src/test.java
+++ src/test.java
@@ -1,19 +1,59 @@
-package com.example;
+package com.example.oldapp;
 
-import org.apache.commons.lang3.StringUtils;
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 
-public class SimpleApp {
+/**
+ * Simple application using outdated Apache Commons Lang and HttpClient libraries
+ * This code will require changes when updating to newer versions
+ */
+public class OldStyleApp {
+    
     public static void main(String[] args) {
-        String text = "Hello, World!";
-        System.out.println(StringUtils.reverse(text));
-        List<String> words = new ArrayList<String>();
-        words.add("Java");
-        words.add("Programming");
+        OldStyleApp app = new OldStyleApp();
+        app.processString("  Hello World  ");
+        app.makeHttpRequest("http://example.com");
+    }
+    
+    /**
+     * Process a string using old Apache Commons Lang (v2.x)
+     */
+    public void processString(String input) {
+        System.out.println("Original input: '" + input + "'");
         
-        for (String word : words) {
-            System.out.println(StringUtils.capitalize(word));
+        // Old style StringUtils methods
+        String trimmed = StringUtils.strip(input);
+        boolean isEmpty = StringUtils.isEmpty(input);
+        boolean isAlpha = StringUtils.isAlpha(trimmed);
+        
+        System.out.println("Trimmed: '" + trimmed + "'");
+        System.out.println("Is empty: " + isEmpty);
+        System.out.println("Is alpha only: " + isAlpha);
+    }
+    
+    /**
+     * Make HTTP request using deprecated HttpClient 4.3.x API
+     */
+    public void makeHttpRequest(String url) {
+        try {
+            // This DefaultHttpClient is deprecated in newer versions
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(url);
+            
+            System.out.println("Making request to: " + url);
+            
+            // Old style response handling
+            HttpResponse response = client.execute(request);
+            String responseBody = EntityUtils.toString(response.getEntity());
+            
+            System.out.println("Response status: " + response.getStatusLine());
+            System.out.println("Response length: " + responseBody.length() + " chars");
+        } catch (Exception e) {
+            System.err.println("Error making HTTP request: " + e.getMessage());
         }
     }
 }
